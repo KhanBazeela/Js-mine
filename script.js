0f0a7e7842f63276393de0570436834db6aa0988
KhanBazeela <bazeelakhan9@gmail.com>
2024-06-24 09:41:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function lifeInWeeks(age) {
+    var yearsRemaining = 90 - age;
+    var days = 365 * yearsRemaining;
+    var weeks = 52 * yearsRemaining;
+    var months = 12 * yearsRemaining;
+
+
+
+
+console.log("You have "+ days +"  days, "+weeks+"  weeks, and "+ years+" months left.");
+}
