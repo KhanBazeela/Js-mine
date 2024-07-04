8454013721f0a3db219ea93c909efaffc99fd002
KhanBazeela <bazeelakhan9@gmail.com>
2024-07-04 13:49:13 +0530

    commit

--- BASICS.js
+++
@@ -1,6 +0,0 @@
-['Devika', 'Mary', 'Jhony', 'Hector'].forEach((varName) => {
-    if (varName.length == 4) {
-        pm.variables.set("putUserName", varName);
-        return; // This exits the forEach loop after setting the variable
-    }
-});
---
+++ newFolder/date.js
@@ -0,0 +1,4 @@
+
+var mom = require('moment');
+let currDate = mom().format("YYYY-MM-DD");
+console.log(currDate);
